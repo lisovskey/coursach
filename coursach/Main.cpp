ef83554af489edac61ed65c03003dfdb175de234
lisovskey <ritofzeed@gmail.com>
2017-01-13 17:16:52 +0300

    main loop

--- coursach/Main.cpp
+++ coursach/Main.cpp
@@ -9,7 +9,28 @@
 #include <string>
 #include <conio.h>
 
-int main(void) {
+void dosmth() {}
 
-	return 0;
+int main(int argc, char* argv[]) {
+	// Main loop
+	while (true) {
+		bool again = false;
+
+		// Choose action loop
+		do switch (_getch()) {
+
+		case '1': dosmth();
+			break;
+
+		case '2': dosmth();
+			break;
+
+		case '3': dosmth();
+			break;
+
+		case '0': return 0;
+		// Exit program
+		default: again = true;
+		} while (again);
+	}
 }
